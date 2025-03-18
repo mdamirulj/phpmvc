49a311d63588a020003155a970140aa4c5ec7c54
Amirul <amiruljamari83@gmail.com>
2025-03-18 15:39:19 +0800

    update home route

--- public/index.php
+++ public/index.php
@@ -3,5 +3,5 @@ require_once '../config.php';
 require_once '../app/Core/Router.php';
 
 $router = new Router();
-$router->get('/', 'HomeController@index');
+$router->get('/phpmvc/public/', 'HomeController@index');
 $router->dispatch();
